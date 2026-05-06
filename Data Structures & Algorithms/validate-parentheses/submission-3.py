148287474d950e6e37443bdbe54fd262b117673d
Cameron Kerestus <125141520+Teamk09@users.noreply.github.com>
2026-05-06 13:42:37 -0400

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.py
@@ -0,0 +1,25 @@
+from collections import deque
+class Solution:
+    def isValid(self, s: str) -> bool:
+        if len(s) % 2 != 0:
+            return False
+
+        stack = deque()
+        pairs = { ")" : "(", "]" : "[", "}" : "{" }
+
+        for let in s:
+            print(let)
+            if let in pairs:
+                print("inside")
+                if stack and stack[-1] == pairs[let]:
+                    print("why not here")
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(let)  
+
+        if not stack:
+            return True
+        else:
+            return False
